b4ff8ed220d45632d14829eba14e15a7e102d0e5
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-03-16 08:50:46 +0200

    create login form view

--- resources/views/loginform.blade.php
+++ resources/views/loginform.blade.php
@@ -1,5 +1,8 @@
 @extends('layout.layout')
 @section('title', 'Login form')
 @section('content')
-
+    <form action="{{url('/')}}" method="post">
+        @csrf
+        @method('post')
+    </form>
 @endsection
