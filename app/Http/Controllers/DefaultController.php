669e15daee3c4a8875d7af9095720259e3a43252
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-03-16 08:47:30 +0200

    create layout

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 
+
+use \Illuminate\Http\Request;
+
 class DefaultController extends Controller
 {
-    public function home()
+    public function home(Request $request)
     {
+        if($request->session()->get('islogged') !== true){
+            return view('loginform');
+        }
 
     }
 }
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@section('title')@show</title>
+</head>
+
+<body>
+@section('content')
+@show
+</body>
+
+</html>
