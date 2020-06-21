0b4ee7c78d85b0e81f6afd92820cd940e6f7569e
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-06-21 14:07:28 +0300

    create migration for 'post' table

---
+++ database/migrations/2020_06_21_104741_create_post_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->collation = 'utf8_general_ci';
+            $table->charset = 'utf8';
+            $table->bigIncrements('id');
+            $table->text('content');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
